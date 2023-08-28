34b328f33eff0b693a0e9744116a5265bd4a1a76
ly <ly137544897@outlook.com>
2023-08-28 18:07:29 +0800

    回调钩子

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 # This is a sample Python script.
 import asyncio
 
-from utils.cv_utils import cut_video_frame, cut_frame_with_audio
-from utils.dy_utils import download_video
+from utils.cv_utils import cut_frame_with_audio
+from utils.dy_utils import DyUtils
 from utils.ffmpeg_utils import video_do
-from utils.bi_utils import BiUtils
-from utils import bi_headers
 from utils.file_utils import download_file
 
 dy_url = "4.61 oDH:/ 复制打开抖音，看看黄泉杂货铺 # 因为一个片段看了整部剧 # 抖音短... https://v.douyin.com/iJvfFUsA/"
 bi_number = "BV1wu4y1R7Le"
 file_save_dy_path = "save/save_dy_video.mp4"
 file_save_bi_path = "save/save_bi_video.mp4"
-
-
-async def dy_download():
-    download_video(dy_url, file_save_dy_path)
 
 
 async def ffmpeg_video():
@@ -26,24 +20,19 @@ async def frame_cut():
     cut_frame_with_audio(file_save_dy_path)
 
 
-def __download(download_url: str, headers):
+def __bi_download(download_url: str, headers):
     download_file(download_url, file_save_bi_path, headers=headers)
     print("download_url is ", download_url)
 
 
+def __dy_download(download_url: str):
+    download_file(download_url, file_save_dy_path)
+    print("download_url is ", download_url)
+
+
 def main():
-    BiUtils(bi_number, __download).bi_download()
-
-
-#
-# file_path = Path(file_save_dy_path)
-# if file_path.is_file():
-#     await ffmpeg_video()
-#     await frame_cut()
-# else:
-#     await dy_download()
-#     await ffmpeg_video()
-#     await frame_cut()
+    # BiUtils(bi_number, __bi_download).bi_download()
+    DyUtils(dy_url, __dy_download).download_video()
 
 
 if __name__ == '__main__':
---
+++ utils/dy_utils/__init__.py
@@ -0,0 +1,66 @@
+import json
+import re
+
+import requests
+
+from urls.urls import Urls
+from utils import dy_headers
+from utils.dy_utils.dy_encipher import getXbogus
+
+
+class DyUtils:
+    def __init__(self, url: str, callback):
+        self.url = url
+        self.aw_id = None
+        self.callback = callback
+
+    def download_video(self, ):
+        # 1 获取短链
+        short_link = self.__get_short_link()
+        print("short link is ", short_link)
+
+        # 2 获取request对象
+        short_link_res = requests.get(short_link, headers=dy_headers)
+
+        # 3 转义获取真实链接
+        real_link = str(short_link_res.request.path_url)
+        print("url_str " + real_link)
+
+        # 4 获取aw_id
+        aw_id = re.findall('video/(\d+)?', real_link)[0]
+        self.aw_id = aw_id
+        print("获取aw_id ", aw_id)
+
+        # 5 通过aweme_id获取信息
+        aw_info = self.__get_aw_info()
+
+        # 6 获取url_list的第一个
+        if aw_info is not None:
+            # 6.1 打印相关信息
+            url_first = aw_info["video"]["play_addr"]["url_list"][0]
+            print("url is ", url_first)
+            self.callback(url_first, )
+
+    # 获取真正的短链
+    def __get_short_link(self):
+        return re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', self.url)[0]
+
+    # 获取作品信息
+    def __get_aw_info(self, ):
+        if self.aw_id is None:
+            return None
+        while True:
+            try:
+                payload = "aw_id=" + self.aw_id + "&device_platform=webapp&aid=6383"
+                print("format_url " + payload)
+                single_video_url = Urls().POST_DETAIL + getXbogus(payload)
+                raw = requests.get(url=single_video_url, headers=dy_headers).text
+                print("get_aw_info raw " + raw)
+                datadict = json.loads(raw)
+                if datadict is not None and datadict["status_code"] == 0:
+                    break
+            except Exception as e:
+                print('[报错] ' + e)
+                return ""
+
+        return datadict["aw_detail"]
